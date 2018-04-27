44345e8dd386ff2d265df86020d26d08045796ca
lal@amt.in <lal@amt.in>
2018-04-27 10:47:01 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var auth = require('http-auth');
+app.set('port', (process.env.PORT || 3000));
+
+var basic = auth.basic({
+        realm: "Simon Area."
+    }, (username, password, callback) => { 
+        // Custom authentication
+        // Use callback(error) if you want to throw async error.
+        callback(username === "shijuaugustinem" && password === "shijusom@889!l");
+    }
+);
+
+//app.use(auth.connect(basic));
+
+
+app.use(express.static(path.join(__dirname, 'prod')));
+app.use('/about',express.static(path.join(__dirname, 'prod')));
+
+
+app.listen(app.get('port'), function () {
+  console.log('app listening on port'+ app.get('port'));
+});
